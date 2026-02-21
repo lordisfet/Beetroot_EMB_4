a1dfc3e9ec3c1295a85a2b1ef68e965b2664a98e
lordisfet <lordisfet@gmail.com>
2026-02-21 03:43:41 +0200

    Add 3 leds and use millis for flashing

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,32 @@
 #include <Arduino.h>
 
+void flashLed(int led, unsigned long start, unsigned int duration, unsigned int currentCycle);
+
 int redLed = 4;
 int blueLed = 17;
+int greenLed = 10;
+unsigned int duration = 1000;
+unsigned int totalCycles = 3000; // Duration for which the LED will be on (in milliseconds)
 
 void setup() {
   pinMode(redLed, OUTPUT);
   pinMode(blueLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(redLed, HIGH);
-  delay(130);
-  digitalWrite(blueLed, LOW);
-  delay(1000);
-  digitalWrite(blueLed, HIGH);   
-  delay(130);                  
-  digitalWrite(redLed, LOW);                 
-  delay(1000);                     
+    unsigned int currentCycle = millis() % totalCycles; // Get the current cycle time
+
+    flashLed(redLed, 0, duration, currentCycle);
+    flashLed(blueLed, 1000, duration, currentCycle);
+    flashLed(greenLed, 2000, duration, currentCycle);
+}
+
+void flashLed(int led, unsigned long start, unsigned int duration, unsigned int currentCycle) {
+  unsigned long now = millis();
+  if (currentCycle >= start && currentCycle < start + duration) {
+    digitalWrite(led, HIGH);
+  } else {
+    digitalWrite(led, LOW);
+  }
 }
