45dfd3272d7ef21c0ee137ba79b5ee81792c9791
lordisfet <lordisfet@gmail.com>
2026-02-25 22:07:06 +0200

    Not important fix

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,13 @@ unsigned long last_interrupt_time = 0;
 int8_t debounce_time = 50; 
 
 void IRAM_ATTR reaction_left() {
-  if (millis() - last_interrupt_time > debounce_time) {
+  unsigned long current_time = millis();
+  
+  if (current_time - last_interrupt_time > debounce_time) {
     counter_left++;  
     Serial.println("\nLEFT Button Pressed! Count: " + String(counter_left));
   }
-  last_interrupt_time = millis();
+  last_interrupt_time = current_time;
 }
 
 void setup() {
