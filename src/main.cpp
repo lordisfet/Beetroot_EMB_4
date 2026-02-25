1ab67c77e29a246444f9637f4b06e5dc63559bcd
lordisfet <lordisfet@gmail.com>
2026-02-25 22:04:11 +0200

    Add ex for module 1-5

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 
+#define BUTTON_LEFT T20
+
+volatile int16_t counter_left = 0;
+
+unsigned long last_interrupt_time = 0;
+int8_t debounce_time = 50; 
+
+void IRAM_ATTR reaction_left() {
+  if (millis() - last_interrupt_time > debounce_time) {
+    counter_left++;  
+    Serial.println("\nLEFT Button Pressed! Count: " + String(counter_left));
+  }
+  last_interrupt_time = millis();
+}
+
 void setup() {
+  pinMode(BUTTON_LEFT, INPUT);
+  Serial.begin(115200);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_LEFT), reaction_left, FALLING);
 }
 
 void loop() {
-  delay(500); // Small delay to prevent excessive CPU usage  
-  Serial.println("Hello, World!");
+
 }
