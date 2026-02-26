3b8524398a35c6a908e7f44535ddd2832ff39cf9
lordisfet <lordisfet@gmail.com>
2026-02-26 18:08:25 +0200

    Add debounce for left and right fronts

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,37 @@
 #include <Arduino.h>
 
-#define BUTTON_LEFT T20
+#define BUTTON T20
 
-volatile int16_t counter_left = 0;
+volatile int16_t counter = 0;
 
 unsigned long last_interrupt_time = 0;
+bool last_button_state = HIGH;
 int8_t debounce_time = 50; 
 
-void IRAM_ATTR reaction_left() {
-  unsigned long current_time = millis();
+void IRAM_ATTR reaction() {
+  if (last_button_state != digitalRead(BUTTON))
+  {
+    if (last_button_state == LOW)
+    {
+      unsigned int current_time = millis();
 
-  if (current_time - last_interrupt_time > debounce_time) {
-    counter_left++;  
-    Serial.println("\nLEFT Button Pressed! Count: " + String(counter_left));
-    last_interrupt_time = current_time;
+      if (current_time - last_interrupt_time > debounce_time)
+      {
+        counter++;
+        last_interrupt_time = current_time;
+        Serial.println("Counter: " + String(counter) + " at " + String((current_time / 1000.0 )) + " s");
+      }
+    }
+
+    last_button_state = !last_button_state;
   }
 }
 
 void setup() {
-  pinMode(BUTTON_LEFT, INPUT_PULLUP);
+  pinMode(BUTTON, INPUT_PULLUP);
   Serial.begin(115200);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_LEFT), reaction_left, FALLING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON), reaction, CHANGE);
 }
 
 void loop() {
-
 }
