67286b380679d3fcec284499e9e922d8b48a2a86
lordisfet <lordisfet@gmail.com>
2026-03-04 02:58:16 +0200

    ADC, RAW, calculated and measured voltage print and error

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,33 @@
 #define ADC_PIN 4
 
 double calculateVoltage(int adcValue);
+void printData(int adcResolution = ADC_RESOLUTION, adc_attenuation_t attenuation = ADC_11db);
 
 int measureInterval = 100;
 
 void setup() {
   Serial.begin(MONITOR_BAUD_RATE);
   analogReadResolution(ADC_RESOLUTION);
+  analogSetAttenuation(ADC_11db); // Set attenuation for the ADC pin to read up to 3.3V
   pinMode(ADC_PIN, INPUT);
 }
 
 void loop() {
-  int adcValue = analogRead(ADC_PIN);
-
-  Serial.print("ADC RAW value: " + String(adcValue));
-  Serial.print("\t|\tCalculeted voltage: " + String(calculateVoltage(adcValue), 3) + " V");
-  Serial.println("\t|\t Measured voltage: " + String(analogReadMilliVolts(ADC_PIN)) + " V");
+  printData();
   delay(measureInterval);
 }
 
 double calculateVoltage(int adcValue) {
   return (adcValue / (double)ADC_MAX_VALUE) * ADC_REF_VOLTAGE;
+}
+
+void printData(int adcResolution = ADC_RESOLUTION, adc_attenuation_t attenuation = ADC_11db) {
+  int adcValue = analogRead(ADC_PIN);
+  double calculatedVoltage = calculateVoltage(adcValue);
+  double measuredVoltage = analogReadMilliVolts(ADC_PIN) / 1000.0;
+
+  Serial.print("ADC RAW value: " + String(adcValue));
+  Serial.print("\t|\tCalculeted voltage: " + String(calculatedVoltage, 3) + " V");
+  Serial.print("\t|\tMeasured voltage: " + String(measuredVoltage, 3) + " V");
+  Serial.println("\t|\tDifference: " + String(abs(calculatedVoltage - measuredVoltage) * 100.0 / measuredVoltage, 2) + " %");
 }
