f584076f4690771d79355aa2578fd5d30671b113
lordisfet <lordisfet@gmail.com>
2026-03-23 12:30:37 +0200

    add relay delay mesearing

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,88 @@
 #include <Arduino.h>
 
-#include "button/Button.h"
-
 constexpr int MONITOR_BOUD_RATE = 115200;
 
-constexpr int8_t BUTTON_PIN = 4;
-constexpr int8_t DEBOUNCE_TIME = 50;
-
 constexpr int8_t RELAY_CONTROLL_PIN = 21;
-bool relayControlPinState = HIGH;
 constexpr int8_t RELAY_INPUT_PIN = 2;
 
-Button button(BUTTON_PIN, DEBOUNCE_TIME);
+volatile unsigned long signalSentTime = 0;
+volatile unsigned long reactionTime = 0;
+volatile bool measurementReady = false;
+volatile bool waitingForMeasurement = false;
+
+const int MAX_MEASUREMENTS = 10;
+int measurementCount = 0;
+unsigned long totalReactionTime = 0;
+bool relayControlPinState = LOW;
+
+void IRAM_ATTR relayContactISR()
+{
+  if (waitingForMeasurement)
+  {
+    reactionTime = millis() - signalSentTime;
+    measurementReady = true;
+    waitingForMeasurement = false;
+  }
+}
 
 void setup()
 {
   Serial.begin(MONITOR_BOUD_RATE);
-
-  button.init();
+  Serial.println("Starting relay reaction time test (10 measurements)...");
 
   pinMode(RELAY_CONTROLL_PIN, OUTPUT);
   digitalWrite(RELAY_CONTROLL_PIN, relayControlPinState);
+
   pinMode(RELAY_INPUT_PIN, INPUT_PULLDOWN);
+
+  attachInterrupt(digitalPinToInterrupt(RELAY_INPUT_PIN), relayContactISR, RISING);
 }
 
 void loop()
 {
   unsigned long currentTime = millis();
   static unsigned long lastActiveTime = 0;
-  if (currentTime - lastActiveTime >= 1000)
+
+  if (measurementCount < MAX_MEASUREMENTS)
   {
-    lastActiveTime = currentTime;
-    relayControlPinState = !relayControlPinState;
-    digitalWrite(RELAY_CONTROLL_PIN, relayControlPinState);
-    Serial.println(relayControlPinState);
+    if (currentTime - lastActiveTime >= 1000)
+    {
+      lastActiveTime = currentTime;
+      relayControlPinState = !relayControlPinState;
+
+      if (relayControlPinState == HIGH)
+      {
+        measurementReady = false;
+        waitingForMeasurement = true;
+        signalSentTime = millis();
+        digitalWrite(RELAY_CONTROLL_PIN, HIGH);
+      }
+      else
+      {
+        digitalWrite(RELAY_CONTROLL_PIN, LOW);
+      }
+    }
+  }
+
+  if (measurementReady)
+  {
+    measurementReady = false;
+    measurementCount++;
+    totalReactionTime += reactionTime;
+
+    Serial.print("Measurement ");
+    Serial.print(measurementCount);
+    Serial.print(": ");
+    Serial.print(reactionTime);
+    Serial.println(" ms");
+
+    if (measurementCount == MAX_MEASUREMENTS)
+    {
+      float averageTime = (float)totalReactionTime / MAX_MEASUREMENTS;
+      Serial.println("================================");
+      Serial.print("Average reaction time: ");
+      Serial.print(averageTime);
+      Serial.println(" ms");
+    }
   }
 }
