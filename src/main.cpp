4d3fb03bf1227fbd3176d0af3d2eaeb09103108d
lordisfet <lordisfet@gmail.com>
2026-03-23 03:54:42 +0200

    Add potentiomente logic

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,11 @@ constexpr int8_t DEBOUNCE_TIME = 50;
 constexpr int8_t PWM_PIN = 45;
 
 constexpr int8_t HANDLE_PIN = 3;
-constexpr int8_t POTENTIOMETER_RESOLUTION = 12;
-constexpr int16_t
+constexpr uint8_t ANALOG_RESOLUTION = 12;
+constexpr uint16_t ANALOG_MAX_VALUE = (1 << ANALOG_RESOLUTION) - 1;
 
-    constexpr int TACT_LENGHT = 20;
-constexpr float DUTY_CYCLE = 1;
+constexpr int TACT_LENGHT = 20;
+constexpr float DUTY_CYCLE = 0.3;
 constexpr DecimalPrefix PREFIX = MS;
 
 Button button(BUTTON_PIN, DEBOUNCE_TIME, UNCHANGED);
@@ -29,6 +29,7 @@ void setup()
   pwm.init();
 
   pinMode(HANDLE_PIN, INPUT_PULLDOWN);
+  analogReadResolution(ANALOG_RESOLUTION);
 }
 
 void loop()
@@ -36,16 +37,17 @@ void loop()
   if (button.getState())
   {
     pwm.tick();
+    // pwm.mapAnalogToDutyCycle(analogReadRaw(HANDLE_PIN), ANALOG_MAX_VALUE);
   }
   else
   {
     pwm.setPWMLevel(LOW);
   }
 
-  // static unsigned long lastPrintTime = 0;
-  // if (millis() - lastPrintTime > 250)
-  // {
-  //   Serial.println((String)analogReadRaw(HANDLE_PIN) + '\t' + (String)button.getState());
-  //   lastPrintTime = millis();
-  // }
+  static unsigned long lastPrintTime = 0;
+  if (millis() - lastPrintTime > 250)
+  {
+    Serial.println((String)analogReadRaw(HANDLE_PIN) + '\t' + (String)pwm.getDutyCycle() + '\t' + (String)button.getState());
+    lastPrintTime = millis();
+  }
 }
